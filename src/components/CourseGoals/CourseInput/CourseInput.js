8a7c07c122f0e330fa77088f4712bc7627d4f46e
Nathalie Fajardo <nathaliefc89@gmail.com>
2023-06-27 13:09:35 +0200

    adding checges in is valid text style

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -8,12 +8,15 @@ const CourseInput = props => {
   const [isValid, setIsValid] = useState(true)
 
   const goalInputChangeHandler = event => {
+    if(event.target.value.trim().length > 0){
+      setIsValid(true)
+    }
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
-    if(enteredValue.trim().length === 0 ){
+    if (enteredValue.trim().length === 0) {
       setIsValid(false)
       return;
     }
@@ -22,9 +25,9 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label style={{color: !isValid ? 'red' : 'black'}}>Course Goal</label>
-        <input style={{borderColor: !isValid ? 'red' : 'black', background: !isValid ? 'salmon' : 'transparent' }} type="text" onChange={goalInputChangeHandler} />
+      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+        <label>Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
